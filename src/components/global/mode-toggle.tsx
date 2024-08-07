4c9e6e3687e339353dc2fbcca19584ba71e8f443
Tarun Vishwakarma <138651451+TarunVishwakarma1@users.noreply.github.com>
2024-08-08 01:24:02 +0530

    [BUGFIX]: Removed System theme as it was causing issue in the links and impacting UI/UX
    [EHN]  changed the favicon for the application

--- src/components/global/mode-toggle.tsx
+++ src/components/global/mode-toggle.tsx
@@ -5,12 +5,6 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "../mode-toggle/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../mode-toggle/dropdown-menu";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
@@ -23,26 +17,17 @@ export function ModeToggle() {
     }
   }, [theme]);
 
+  const toggleTheme = () => {
+    // Toggle between light and dark
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+    // removed the system theme change
   );
 }
